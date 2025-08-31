64cd4c3f9e8dbcfef02f56e0e4be985a432b5bf2
jose <joliher@github.com>
2025-08-31 03:31:47 +0200

    Archivo install rehecho

--- install.sh
+++ install.sh
@@ -8,75 +8,163 @@ fi
 ###########################
 # Definición de variables #
 ###########################
-programas=('bspwm' 'sxhkd' 'kitty' 'picom' 'polybar' 'tmux')
+usuario=$(logname)
+home_usuario=$(grep "^jose:" /etc/passwd | cut -d: -f6)
+
+programas=('bspwm' 'sxhkd' 'kitty' 'picom' 'polybar' 'tmux' 'pulseaudio' 'curl' 'git')
 programas_no_apt=('starship')
-opcional=('curl' 'tar' 'bzip2' 'wget')
+opcional=('tar' 'bzip2' 'wget')
 
 function install_check () {
   read -p "Deseas proseguir con la instalación? y/N: " quest1
 
   if [ "${quest1,,}" != "y" ]; then
 	echo "Abortando..."
-	exit 1
+	return 1
   else
 	read -p "Deseas proseguir con la instalación? (double check) y/N: " quest2
   	if [ "${quest2,,}" != "y" ]; then
 		echo "Abortando..."
-		exit 1
+		return 1
 	fi
   fi
 
   echo "Procediendo a la instalación..."
 }
 
+function check_programs () {
+	por_instalar=()
+	check_program=0
+
+	for i in ${programas[@]}; do
+		if ! dpkg -l $i &>/dev/null; then
+			por_instalar+=("$i")
+			check_program=1
+		fi
+	done
+	if ! which starship &>/dev/null; then
+		por_instalar+=('starship')
+		check_program=1
+	fi
+
+	if [ $check_program -eq 1 ]; then
+		return 1
+	fi
+	
+	return 0
+}
+
 ########
 # Main #
 ########
-echo "Se van a instalar los siguientes programas:"
-for i in ${programas[@]}; do
-	echo -e " -$i"
+
+while true; do
+
+  echo "=============================="
+  echo "        MENÚ PRINCIPAL        "
+  echo "=============================="
+  echo "1) Instalar entorno"
+  echo "2) Instalar opcionales"
+  echo "3) Configurar entorno"
+  echo "4) Comprobar instalaciones"
+  echo "5) Ver configuración"
+  echo "6) Salir"
+  echo "=============================="
+
+  read -p "Elige una opción [1-6]: " opt
+
+  case $opt in
+	1)
+		echo "Se van a instalar los siguientes programas:"
+		for i in ${programas[@]}; do
+			echo -e " -$i"
+		done
+		for i in ${programas_no_apt[@]}; do
+			echo -e " -$i"
+		done
+
+		if install_check; then
+			apt install ${programas[@]} ${opcional[@]}
+			# Instalación de starship
+			if ! which starship &>/dev/null; then
+				curl -sS https://starship.rs/install.sh | sh
+			else
+				echo "Starship ya se encuentra instalado"
+			fi
+		fi
+
+		;;
+	2)
+		echo "Se van a instalar los siguientes programas:"
+		for i in ${opcional[@]}; do
+			echo -e " -$i"
+		done
+
+		if install_check; then
+			apt install ${opcional[@]}
+		fi
+
+		;;
+	3)
+		if ! check_programs; then
+			echo "Faltan programas por instalar"
+			break
+		fi
+
+		cd /tmp && git clone https://github.com/joliher/entorno_bspwm &>/dev/null
+		ruta_repo=/tmp/entorno_bspwm
+		chown -R $usuario: $ruta_repo && chmod ug+x $ruta_repo/.scripts/*
+	  
+	  # Configuración de bspwm
+	  	mkdir -p $home_usuario/.config/bspwm/
+		chmod +x $ruta_repo/bspwm/bspwmrc
+		mv $ruta_repo/bspwm/* $home_usuario/.config/bspwm/
+
+	  # Configuración de sxhkd
+		mkdir -p $home_usuario/.config/sxhkd/
+		mv $ruta_repo/sxhkd/* $home_usuario/.config/sxhkd/
+
+	  # Configuración de kitty
+	  	mv $ruta_repo/kitty/* $home_usuario/.config/
+
+	  # Configuración de picom
+		mv $ruta_repo/picom/* $home_usuario/.config/
+
+	  # Configuración de polybar
+		mv $ruta_repo/polybar/* /etc/polybar/
+
+	  # Configuración de starship
+	  if ! grep -qw 'eval "$(starship init bash)"' ~/.bashrc
+	  then
+		echo "#Configuración de starship" >> ~/.bashrc
+		echo -e 'eval "$(starship init bash)" \n' >> ~/.bashrc
+		echo "Starship configurado"
+	  fi
+
+	  # Configuración de tmux
+		mv $ruta_repo/tmux/* $home_usuario/
+
+	  # Scripts
+	  	mkdir -p $home_usuario/.scripts/ 
+		mv $ruta_repo/.scripts/* $home_usuario/.scripts/
+	
+	  rm -rf $ruta_repo
+		;;
+	4)
+
+		;;
+	5)
+
+		;;
+	6)
+		echo "Saliendo"
+		exit 0
+		;;
+	*)
+		
+		;;
+  esac
 done
-for i in ${programas_no_apt[@]}; do
-	echo -e " -$i"
-done
-echo "Así como las siguientes dependencias:"
-for i in ${opcional[@]}; do
-	echo -e " -$i"
-done
-install_check
 
-apt install ${programas[@]} ${opcional[@]}
-
-echo -e "\nA continuación se van a instalar los programas que no se pueden instalar mediante APT"
-read -n1 -s -r -p "Presione una tecla para continuar..." && echo ""
-
-# Instalación de starship
-curl -sS https://starship.rs/install.sh | sh
-
-###################
-# Configuraciones #
-###################
-
-# Configuración de bspwm
-
-# Configuración de sxhkd
-
-# Configuración de kitty
-
-# Configuración de picom
-
-# Configuración de polybar
-
-# Configuración de starship
-if ! grep -qw 'eval "$(starship init bash)"' ~/.bashrc && which starship &>/dev/null
-then
-	echo "#Configuración de starship" >> ~/.bashrc
-	echo -e 'eval "$(starship init bash)" \n' >> ~/.bashrc
-fi
-
-# Configuración de tmux
-
-###########
-# Scripts #
-###########
+echo ""
 

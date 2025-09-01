272e6baf52da313e0a9a99609e63afb703a61692
jose <joliher@github.com>
2025-09-01 19:12:36 +0200

    Cambios a install.sh V2

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-if [ $(whoami) != "root" ]; then
-	echo "Debes ejecutar este script como root"
-	exit 1
-fi
-
 ###########################
 # Definición de variables #
 ###########################
 
-usuario=$(logname)
-programas=('bspwm' 'sxhkd' 'kitty' 'picom' 'polybar' 'tmux' 'pulseaudio' 'curl' 'git' 'lightdm' 'lightdm-gtk-greeter')
+programas=('bspwm' 'sxhkd' 'kitty' 'picom' 'polybar' 'tmux' 'pulseaudio' 'curl' 'git')
 programas_no_apt=('starship')
-opcional=('tar' 'bzip2' 'wget')
+opcional=('tar' 'bzip2' 'wget' 'lightdm' 'lightdm-gtk-greeter' 'vim')
+
+USE_LOCAL=false
+
+while [ $# -gt 0 ]; do
+	case $1 in
+		--local) USE_LOCAL=true ;;
+	esac
+	shift
+done  
+
 
 function install_proceed () {
   read -p "Deseas proseguir con la instalación? y/N: " quest1
@@ -36,7 +40,7 @@ function check_programs () {
 	error=0
 
 	for i in ${programas[@]}; do
-		if ! dpkg -l $i &>/dev/null; then
+		if ! command -v $i &>/dev/null; then
 			por_instalar+=("$i")
 			error=1
 		fi
@@ -83,7 +87,7 @@ while true; do
 	  done
 
 	  if install_proceed; then
-		apt install ${programas[@]} ${opcional[@]}
+		sudo apt install ${programas[@]}
 		# Instalación de starship
 		if ! which starship &>/dev/null; then
 			curl -sS https://starship.rs/install.sh | sh
@@ -99,60 +103,133 @@ while true; do
 	  done
 
 	  if install_proceed; then
-		apt install ${opcional[@]}
+		sudo apt install ${opcional[@]}
 	  fi
 		;;
 	3)
-	  if ! check_programs; then
+	if ! check_programs; then
 		echo "Faltan programas por instalar"
-		break
-	  fi
+	fi
 
-sudo -u "$usuario" bash << 'EOF'
-	
-	  # Preparación
-		cd /tmp && git clone https://github.com/joliher/entorno_bspwm &>/dev/null
+	# Preparación
+	  cd $(dirname $0)
+	if [ $USE_LOCAL == "true" ]; then
+		echo "Usando repositorio local en $(pwd)..."
+		echo "Asegurese de todos los archivos de configuración necesarios existan"
+		ruta_repo=$(pwd)
+		POST_CLEAN=false
+	else
+	  	cd /tmp && git clone https://github.com/joliher/entorno_bspwm &>/dev/null
 		ruta_repo=/tmp/entorno_bspwm
-	  
-	  # Configuración de bspwm
-		mkdir $HOME/.config/bspwm/ &>/dev/null
-		mv $ruta_repo/bspwm/* $HOME/.config/bspwm/
-		chmod +x $HOME/.config/bspwm/bspwmrc
+		POST_CLEAN=true
+	fi
 
-	  # Configuración de sxhkd
-	  	mkdir $HOME/.config/sxhkd/ &>/dev/null
-		mv $ruta_repo/sxhkd/* $HOME/.config/sxhkd/
-
-	  # Configuración de kitty
-		mv $ruta_repo/kitty/* $HOME/.config/
-
-	  # Configuración de picom
-		mv $ruta_repo/picom/* $HOME/.config/
-
-	   # Configuración de starship
-		if ! grep -qw 'eval "$(starship init bash)"' ~/.bashrc; then
-		  echo "#Configuración de starship" >> ~/.bashrc
-		  echo -e 'eval "$(starship init bash)" \n' >> ~/.bashrc
-		  echo "Starship configurado"
+	# Configuración de bspwm
+	  echo "Configurando BSPWM..."
+	  mkdir -p $HOME/.config/bspwm/ &>/dev/null
+	if [ -d $ruta_repo/bspwm/ ]; then
+		if cp -r $ruta_repo/bspwm/* $HOME/.config/bspwm/ && chmod +x $HOME/.config/bspwm/bspwmrc; then
+			echo "BSPWM configurado correctamente"
+		else
+			echo "No se ha podido configurar BSPWM."
 		fi
+	else
+		echo "No se ha podido configurar BSPWM."
+	fi
+	  echo ""
 
-	  # Configuración de tmux
-		mv $ruta_repo/tmux/* $HOME/
+	# Configuración de sxhkd
+	  echo "Configurando SXHKD..."
+	  mkdir -p $HOME/.config/sxhkd/ &>/dev/null
+	if [ -d $ruta_repo/sxhkd ]; then
+		if cp -r $ruta_repo/sxhkd/* $HOME/.config/sxhkd/; then
+			echo "SXHKD configurado correctamente"
+		else
+			echo "No se ha podido configurar SXHKD"
+		fi
+	else
+		  echo "No se ha podido configurar SXHKD"
+	fi
+	  echo ""
 
-	  # Scripts
-	  	mkdir $HOME/.scripts/ &>/dev/null
-		mv $ruta_repo/.scripts/* $HOME/.scripts/
-		chmod ug+x $HOME/.scripts/*
+	# Configuración de kitty
+	  echo "Configurando KITTY"
+	if [ -d $ruta_repo/kitty/ ]; then
+		if cp -r $ruta_repo/kitty/* $HOME/.config/; then
+			echo "KITTY configurado correctamente"
+		else
+			echo "No se ha podido configurar KITTY"
+		fi
+	else
+		echo "No se ha podido configurar KITTY"
+	fi
+	  echo ""
+
+	# Configuración de picom
+	  echo "Configurando picom"
+	if [ -d $ruta_repo/picom/ ]; then
+		if cp -r $ruta_repo/picom/* $HOME/.config/; then
+			echo "PICOM configurado correctamente"
+		else
+			echo "No se ha podido configurar PICOM"
+		fi
+	else
+		echo "No se ha podido configurar PICOM"
+	fi
+	  echo ""
+
+	# Configuración de polybar
+	  echo "Configurando POLYBAR"
+	  echo "Se requieren permisos de sudo para configurar POLYBAR"
+	if [ -d /etc/polybar/ ] && sudo cp -r $ruta_repo/polybar/* /etc/polybar/; then
+		echo "POLYBAR configurado correctamente"
+	else
+		echo "No se ha podido configurar POLYBAR"
+	fi
+	  echo ""
+
+	# Configuración de starship
+	  echo "Configurando STARSHIP"
+	if ! grep -qw 'eval "$(starship init bash)"' ~/.bashrc; then
+		echo "#Configuración de starship" >> ~/.bashrc
+		echo -e 'eval "$(starship init bash)" \n' >> ~/.bashrc
+		echo "STARSHIP configurado correctamente"
+	else
+		echo "STARSHIP ya se encuentra configurado. No se realizará ninguna acción adicional"
+	fi
+	  echo ""
+
+	# Configuración de tmux
+	  echo "Configurando TMUX..."
+	if [ -d $ruta_repo/tmux/ ]; then
+		if cp -r $ruta_repo/tmux/* $HOME/; then
+			echo "TMUX configurado correctamente"
+		else
+			echo "No se ha podido configurar TMUX"
+		fi
+	else
+		echo "No se ha podido configurar TMUX"
+	fi
+	  echo ""
+
+	# Scripts
+	  echo "Configurando SCRIPTS..."
+	  mkdir $HOME/.scripts/ &>/dev/null
+	if [ -d $ruta_repo/.scripts/ ]; then
+		if cp -r $ruta_repo/.scripts/* $HOME/.scripts/ && chmod ug+x $HOME/.scripts/*; then
+			echo "SCRIPTS configurados correctamente"
+		else
+			echo "No se han podido configurar los SCRIPTS"
+		fi
+	else
+		echo "No se han podido configurar los SCRIPTS"
+	fi
+	 echo ""
 	
-EOF
-
-	  # Configuración de polybar
-		mv /tmp/entorno_bspwm/polybar/* /etc/polybar/
-
-	  # Limpieza
-		rm -rf /tmp/entorno_bspwm/ &>/dev/null
-
-
+	# Limpieza
+	  if [ $POST_CLEAN == "true" ]; then
+		  rm -rf /tmp/entorno_bspwm/ &>/dev/null
+	  fi
 		;;
 	4)
 
